51860a2b1e43e86422c36dc11046acc4656f73ea
Markus Winter <markus@wntr.me>
2018-11-17 23:19:05 +0100

    Disable test on linux

--- Tests/VectorTests/XCTestManifests.swift
+++ Tests/VectorTests/XCTestManifests.swift
@@ -1,17 +1,11 @@
 import XCTest
 
-#if !os(macOS) 
-    #if canImport(simd)
-    public func allTests() -> [XCTestCaseEntry] {
-        return [
-            testCase(Vector2Tests.allTests),
-            testCase(Vector3Tests.allTests),
-            testCase(Vector4Tests.allTests)
-        ]
-    }
-    #else 
-    public func allTests() -> [XCTestCaseEntry] {
-        return []
-    }
-    #endif
+#if !os(macOS)
+public func allTests() -> [XCTestCaseEntry] {
+    return [
+        testCase(Vector2Tests.allTests),
+        testCase(Vector3Tests.allTests),
+        testCase(Vector4Tests.allTests)
+    ]
+}
 #endif

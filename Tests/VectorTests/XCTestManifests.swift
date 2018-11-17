32ffae7ebbb3087f561ae6997454d38ff76e7fe4
Markus Winter <markus@wntr.me>
2018-11-17 23:12:07 +0100

    Disable test on not simd hardware

--- Tests/VectorTests/XCTestManifests.swift
+++ Tests/VectorTests/XCTestManifests.swift
@@ -1,11 +1,17 @@
 import XCTest
 
-#if !os(macOS)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(Vector2Tests.allTests),
-        testCase(Vector3Tests.allTests),
-        testCase(Vector4Tests.allTests)
-    ]
-}
+#if !os(macOS) 
+    #if canImport(simd)
+    public func allTests() -> [XCTestCaseEntry] {
+        return [
+            testCase(Vector2Tests.allTests),
+            testCase(Vector3Tests.allTests),
+            testCase(Vector4Tests.allTests)
+        ]
+    }
+    #else 
+    public func allTests() -> [XCTestCaseEntry] {
+        return []
+    }
+    #endif
 #endif

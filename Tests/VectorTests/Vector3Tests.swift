ad0672696fb98b3deaa00144d91a050085fce322
Markus Winter <markus@wntr.me>
2018-11-29 23:04:25 +0100

    Updated tests

--- Tests/VectorTests/Vector3Tests.swift
+++ Tests/VectorTests/Vector3Tests.swift
@@ -1,28 +1,45 @@
 import XCTest
 @testable import Vector
 
+public struct TestVector3<Scalar>: Vector3, Equatable where Scalar: Numeric & RawVectorizable3, Scalar.RawVector3: Equatable {
+
+    public typealias Index = VectorIndex3
+    public typealias Element = Scalar
+    public typealias Vector = Element.RawVector3
+
+    /// The underlying optimized vector
+    public var _vector: Vector
+
+    /// Initializes `self` with given vector as underlying optimized vector
+    ///
+    /// - Parameter vector: The vector to mask behind `self`.
+    @_transparent public init(vector: Vector) {
+        _vector = vector
+    }
+}
+
 final class Vector3Tests: XCTestCase {
 
     func testZeroScalar() {
-        let zeroScalar = Vector3<Int32>(repeating: 0)
+        let zeroScalar = TestVector3<Int32>(repeating: 0)
         XCTAssertEqual(zeroScalar, .zero)
     }
 
     func testZeroCollection() {
-        let zeroCollection = Vector3<Int32>(sequence: [0, 0, 0])
+        let zeroCollection = TestVector3<Int32>(sequence: [0, 0, 0])
         XCTAssertEqual(zeroCollection, .zero)
     }
 
     func testZeroArrayLiteral() {
-        let zeroArrayLiteral: Vector3<Int32> = [0, 0, 0]
+        let zeroArrayLiteral: TestVector3<Int32> = [0, 0, 0]
         XCTAssertEqual(zeroArrayLiteral, .zero)
     }
 
     func testDefaultImplementations() {
-        XCTAssertNotNil(Vector3<Int32>(repeating: 3))
-        XCTAssertNotNil(Vector3<UInt32>(repeating: 3))
-        XCTAssertNotNil(Vector3<Float>(repeating: 3))
-        XCTAssertNotNil(Vector3<Double>(repeating: 3))
+        XCTAssertNotNil(TestVector3<Int32>(repeating: 3))
+        XCTAssertNotNil(TestVector3<UInt32>(repeating: 3))
+        XCTAssertNotNil(TestVector3<Float>(repeating: 3))
+        XCTAssertNotNil(TestVector3<Double>(repeating: 3))
     }
 
     static var allTests = [
--- Tests/VectorTests/Vector4Tests.swift
+++ Tests/VectorTests/Vector4Tests.swift
@@ -1,28 +1,45 @@
 import XCTest
 @testable import Vector
 
+public struct TestVector4<Scalar>: Vector4, Equatable where Scalar: Numeric & RawVectorizable4, Scalar.RawVector4: Equatable {
+
+    public typealias Index = VectorIndex4
+    public typealias Element = Scalar
+    public typealias Vector = Element.RawVector4
+
+    /// The underlying optimized vector
+    public var _vector: Vector
+
+    /// Initializes `self` with given vector as underlying optimized vector
+    ///
+    /// - Parameter vector: The vector to mask behind `self`.
+    @_transparent public init(vector: Vector) {
+        _vector = vector
+    }
+}
+
 final class Vector4Tests: XCTestCase {
 
     func testZeroScalar() {
-        let zeroScalar = Vector4<Int32>(repeating: 0)
+        let zeroScalar = TestVector4<Int32>(repeating: 0)
         XCTAssertEqual(zeroScalar, .zero)
     }
 
     func testZeroCollection() {
-        let zeroCollection = Vector4<Int32>(sequence: [0, 0, 0, 0])
+        let zeroCollection = TestVector4<Int32>(sequence: [0, 0, 0, 0])
         XCTAssertEqual(zeroCollection, .zero)
     }
 
     func testZeroArrayLiteral() {
-        let zeroArrayLiteral: Vector4<Int32> = [0, 0, 0, 0]
+        let zeroArrayLiteral: TestVector4<Int32> = [0, 0, 0, 0]
         XCTAssertEqual(zeroArrayLiteral, .zero)
     }
 
     func testDefaultImplementations() {
-        XCTAssertNotNil(Vector4<Int32>(repeating: 4))
-        XCTAssertNotNil(Vector4<UInt32>(repeating: 4))
-        XCTAssertNotNil(Vector4<Float>(repeating: 4))
-        XCTAssertNotNil(Vector4<Double>(repeating: 4))
+        XCTAssertNotNil(TestVector4<Int32>(repeating: 4))
+        XCTAssertNotNil(TestVector4<UInt32>(repeating: 4))
+        XCTAssertNotNil(TestVector4<Float>(repeating: 4))
+        XCTAssertNotNil(TestVector4<Double>(repeating: 4))
     }
 
     static var allTests = [

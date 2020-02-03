a8db1e52edafa291e23c951ae3b84f98907e6c24
Markus Winter <markus@wntr.me>
2020-02-03 10:41:04 +0100

    Add apple/swift-numerics dependency

--- Package.swift
+++ Package.swift
@@ -6,6 +6,9 @@ let package = Package(
     products: [
         .library(name: "Vector", targets: ["Vector"])
     ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-numerics/", from: "0.0.4")
+    ],
     targets: [
         .target(name: "Vector"),
         .testTarget(name: "VectorTests", dependencies: ["Vector"])
